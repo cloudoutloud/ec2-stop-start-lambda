f425226b552d9bbe1c0b9b9649d4c19b2d17c4eb
cloudoutloud <seanrigby26@gmail.com>
2019-09-23 09:21:05 +0100

    Add remote state

---
+++ terraform/remote-state.tf
@@ -0,0 +1,7 @@
+#terraform {
+  #backend "s3" {
+    #bucket = "terraform-state-storage-s3-sslambda"
+    #key    = "terraform-state/terraform.tfstate"
+    #region = "eu-west-2"
+  #}
+#}
---
+++ terraform/s3.tf
@@ -0,0 +1,8 @@
+# #Create an S3 bucket to store the state file.
+# resource "aws_s3_bucket" "terraform-state" {
+#     bucket = "terraform-state-storage-s3-sslambda"
+ 
+#     versioning {
+#       enabled = true
+#     }
+# }
